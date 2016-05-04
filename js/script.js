4b8ae0781427f1d5681735e4f2ae29ec5adf122d
rich-farr <scarletmanuka@gmail.com>
2016-05-05 10:17:04 +1200

    Rebuild track and js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,87 @@
 
-var startRace = document.getElementById("start_race");
-var carOne = document.getElementById("car1");
-var trackOne = document.getElementById("player1_strip");
-var carTwo = document.getElementById("car2");
-var trackTwo = document.getElementById("player2_strip");
+// var startButton = document.getElementById("start_button"); //start_race is the Start button
+// var trackOne = document.getElementById("player1_strip");
+// var moves = trackOne.querySelectorAll("td");
 
-startRace.addEventListener("click", function() {
-  if (carOne.className === "hide") {
-    carOne.className = "";
-  } else carOne.className = "hide";
-});
+// var carOne = document.getElementById("car1");
+
+// // startButton.addEventListener ("click", function() {
+// //   if (carOne.className === "hide") {
+// //     carOne.className = "";
+// //   } else carOne.className = "hide";
+// // });
+
+// function moveKeypress(event) {
+//   if(event.which === 13) {
+//     nextElement = nextElementSibling.innerHTML.carOne;
+//   }
+// };
+
+// click start race button
+//   car appears in td
+//   hit Enter
+//     class Active moves to next td
+//       car moves with class Active
+
+//   var player1 = document.getElementById('player1_strip');
+//   var player2 = document.getElementById('player2_strip');
+
+//   var player1_moves = player1.querySelectorAll('td');
+//   var player2_moves = player2.querySelectorAll('td');
+
+// // Set start state of the game
+// function startGame(boing) {
+//   for (var i = 0; i < boing.length; i++) {
+//     // boing[i].className = "";
+//     boing[0].className = "active";
+//   };
+// };
+
+// startGame(player1_moves);
+// startGame(player2_moves);
+
+// // update player position function
+// function updatePlayerPosition(player) {
+//   for(var i = 0; i < player.length; i++ ) {
+//     if(player[i].className === "active") {
+//       nextElement = player[i].nextElementSibling;
+//       if (player[i] === player[3]) {
+//         alert("Game Over")
+//         startGame(player1_moves);
+//         startGame(player2_moves);
+//       } else {
+//         nextElement.className = "active";
+//         player[i].className = "";
+//       };
+//     break;
+//     };
+//   };
+// };
+
+// // run the update player function based on the key press
+// document.onkeyup = function(e) {
+//   if (e.which === 81) {
+//     updatePlayerPosition(player1_moves)
+//   } else if (e.which === 80) {
+//     updatePlayerPosition(player2_moves)
+//   }
+// };
+
+var playerOne = document.getElementById('player_1');
+var playerTwo = document.getElementById('player_2');
+var carPos = 0;
+
+function moveCar(e) {
+  if (e.keyCode == 39) {
+    carPos += 2;
+    playerOne.style.left = carPos + 'em';
+  }
+
+  if (e.keyCode == 37) {
+    carPos -= 2;
+    playerOne.style.left = carPos + 'em';
+  }
+
+};
+
+document.onkeyup = moveCar;
