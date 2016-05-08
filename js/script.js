609d261203748c2e0029070d2d6e61d87c25df89
rich-farr <scarletmanuka@gmail.com>
2016-05-08 16:15:57 +1200

    Add working game and new textures

--- js/script.js
+++ js/script.js
@@ -5,20 +5,32 @@ function updatePlayerPosition(player_number) {
 
   var arrayIndex = player_number - 1;
   var position = positions[arrayIndex];
-  position += 2;
 
-  player.style.left = position + '%';
-  positions[arrayIndex] = position;
-
+  if (position >= 88) {
+    alert('Player ' + player_number + ' wins!');
+    resetGame();
+  } else {
+    position += 1;
+    player.style.left = position + '%';
+    positions[arrayIndex] = position;
+  }
+  
+  function resetGame () {
+    for (var i = 0; i < positions.length; i++) {
+      positions[i] = 0;
+      player_1.style.left = 0;
+      player_2.style.left = 0;
+    }
+  }
 }
 
 document.onkeyup = function (e) {
   switch (e.which) {
-    case 17:
+    case 81:
       updatePlayerPosition(1);
       break;
-    case 13:
+    case 80:
       updatePlayerPosition(2);
       break;
   }
-};
+}
