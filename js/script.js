4d9db06391896052463a80443893dd56b5ae09e5
rich-farr <scarletmanuka@gmail.com>
2016-05-25 12:03:18 +1200

    Add cars when space is pressed

--- js/script.js
+++ js/script.js
@@ -1,5 +1,30 @@
 var positions = [0, 0];
 
+// var el = document.getElementsByClassName("hide-car");
+
+// el.addEventListener("keypress", function(startGame));
+
+// function startGame() {
+//   console.log("omglolwtf");
+//   var newClass = document.getElementsByClassName("hide-car");
+//   for (var i = 0; i < newClass.length; i++) {
+//     newClass[0].className = "car";
+//   }
+// }
+
+
+function startGame() {
+  for (var i = 0; i < 2; i++) {
+    var cars = ['red', 'yellow'];
+    var playerCar = "player_" + (i + 1);
+    var addCar = document.getElementById(playerCar);
+    addCar.innerHTML = '<img src="img\/' + cars[i] + '_car.png" class="car">';
+    console.log(addCar);
+    // var addCar = document.getElementBy("hide-car");
+    // newClass[0].className = "car";
+  }
+}
+
 function updatePlayerPosition(player_number) {
   var player = document.getElementById('player_' + player_number)
 
@@ -26,6 +51,9 @@ function updatePlayerPosition(player_number) {
 
 document.onkeyup = function (e) {
   switch (e.which) {
+    case 32:
+      startGame();
+      break;
     case 81:
       updatePlayerPosition(1);
       break;
